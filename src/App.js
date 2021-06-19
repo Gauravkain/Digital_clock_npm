51c0c2f8a8a37d1e77133aa7e0e25c52cbc1c762
Gaurav Kumar <55948253+Gauravkain@users.noreply.github.com>
2021-06-20 01:51:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Clock from "react-digital-clock"
+
+const App = () => {
+    return (
+        <h1>
+            <Clock />
+        </h1>
+    )
+};
+
+export default App;
